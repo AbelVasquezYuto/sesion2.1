6da4bd5a0c84f48526e852f544111cdee966e6c1
AbelVasquezYuto <abelfebrero25@gmail.com>
2017-09-17 14:52:08 -0500

    putExtra

--- app/src/main/java/com/galaxy/myapplication/Main2Activity.java
+++ app/src/main/java/com/galaxy/myapplication/Main2Activity.java
@@ -1,19 +1,36 @@
 package com.galaxy.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity{
+
+    private EditText mEtTexto;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        mEtTexto = (EditText) findViewById(R.id.et_texto);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mEtTexto.setText("");
+    }
 
+    public void onNext(View view){
+        String texto = mEtTexto.getText().toString();
 
+        Intent intent = new Intent(this,Main3Activity.class);
+        intent.putExtra("para1",texto);
+        startActivity(intent);
     }
 }
 
--- app/src/main/java/com/galaxy/myapplication/Main3Activity.java
+++ app/src/main/java/com/galaxy/myapplication/Main3Activity.java
@@ -2,12 +2,26 @@ package com.galaxy.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
+
+    private TextView mTvTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        mTvTexto = (TextView) findViewById(R.id.tv_texto);
+
+        String textoRecibido = getIntent().getStringExtra("para1");
+        mTvTexto.setText(textoRecibido);
+
+    }
+
+    public void onBack(View view){
+        finish();
     }
 }
